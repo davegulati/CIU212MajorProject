52e4222c02232b25eea962587406df66aa59d817
Qxintero <1005829@student.sae.edu.au>
2017-06-11 15:26:27 +1000

    Loot Table System foundation created.

---
+++ CIU212MajorProject2D/Assets/Scripts/Loot System/Loot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+	[System.Serializable]
+	public class DropItem
+	{
+		public string name;
+		public GameObject item;
+		public int dropRate;
+	}
+
+	public List <DropItem> LootTable = new List<DropItem>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
