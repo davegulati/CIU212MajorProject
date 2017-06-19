871ed20e1aeb3ececb5c5b857d9eb82ef0cf6a0d
Dave Gulati <1006106@student.sae.edu.au>
2017-06-20 00:34:47 +1000

    See commit message number 2.6 in dev log on google docs for more info.

---
+++ CIU212MajorProject2D/Assets/Scripts/Bud/Bud.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bud : MonoBehaviour {
+
+    private GameObject sen;
+    private float budSpeed;
+    private float maxDistanceFromSen = 2.0f;
+
+    //private Animator anim;
+
+    private void Awake()
+    {
+        sen = GameObject.Find("Sen");
+        //anim = gameobject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+		float distance = Vector2.Distance(transform.position, sen.transform.position);
+        budSpeed = distance;
+		if (distance > maxDistanceFromSen)
+		{
+			BudFollowSen();
+		}
+        else if (distance < maxDistanceFromSen)
+        {
+            //anim.SetBool ("Float", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            UseActiveAbility();
+        }
+    }
+
+    public void BudSpawn ()
+    {
+        
+    }
+
+    public void BudFollowSen ()
+    {
+		transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), sen.transform.position, budSpeed * Time.deltaTime);
+        //anim.SetBool ("Fly", true);
+    }
+
+	public void SetActiveAbility()
+	{
+
+	}
+
+    public void UseActiveAbility ()
+    {
+        Debug.Log("Bud's active ability not set!"); // For now.
+    }
+
+    public void BudDespawn ()
+    {
+        
+    }
+}
