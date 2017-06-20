0c13863c9e545cb6343a6d052b1fc13688191d4c
Hassain Hussein <1005114@student.sae.edu.au>
2017-06-20 17:31:38 +1000

    Just added my files for the currency system into the 3D version, will move to 2D once inventory is finished.

---
+++ CIU212MajorProject3D/Assets/Scenes/Testing Scenes/currencySystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class currencySystem : MonoBehaviour
+{
+
+    public int money;
+
+    public Text moneyCounter;
+
+	// Use this for initialization
+	void Start ()
+    {
+        money = 100;
+        moneyCounter.text = "Money: " + money.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void addMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+        moneyCounter.text = "Money: " + money.ToString();
+    }
+
+    public void subtractMoney(int moneyToSubtract)
+    {
+        if(money - moneyToSubtract < 0)
+        {
+            Debug.Log("Not Enough Money");
+        }
+
+        else
+        {
+            money -= moneyToSubtract;
+            moneyCounter.text = "Money: " + money.ToString();
+        }
+    }
+}
