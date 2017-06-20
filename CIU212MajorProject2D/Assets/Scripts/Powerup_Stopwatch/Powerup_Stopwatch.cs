07918df8902938ec675df362a2ca6b8aef5b8b13
Dave Gulati <1006106@student.sae.edu.au>
2017-06-20 20:13:01 +1000

    See commit message number 3.1 in dev log doc on google drive for more info.

---
+++ CIU212MajorProject2D/Assets/Scripts/Powerup_Stopwatch/Powerup_Stopwatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_Stopwatch : MonoBehaviour {
+
+    private GameObject sen;
+	private float activationRange = 0.8f;
+    private float normalTimeScale = 1.0f;
+    private float slowMotionTimeScale = 0.4f;
+    private float resetAfterSeconds = 3.0f;
+
+    private void Awake()
+    {
+        sen = GameObject.Find("Sen");
+    }
+
+    private void Update()
+	{
+		float distance = Vector2.Distance(transform.position, sen.transform.position);
+		if (distance < activationRange && Input.GetKeyDown(KeyCode.E))
+		{
+            Time.timeScale = slowMotionTimeScale;
+            StartCoroutine(ResetTimeAfterSeconds());
+		}
+	}
+
+    IEnumerator ResetTimeAfterSeconds ()
+    {
+        yield return new WaitForSeconds(resetAfterSeconds);
+        Time.timeScale = normalTimeScale;
+    }
+}
