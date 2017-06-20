9426c16ce6d1a991f40be7002da338f2ad4b66a6
Dave Gulati <1006106@student.sae.edu.au>
2017-06-20 13:00:58 +1000

    See commit message number 2.7 in dev log on google drive for more info.

---
+++ CIU212MajorProject2D/Assets/Scripts/Powerup_HSP/Powerup_HSP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_HSP : MonoBehaviour {
+
+	private GameObject sen;
+	private float activationRange = 0.8f;
+	private float amountToIncreaseBy = 30.0f;
+
+	private void Awake()
+	{
+		sen = GameObject.Find("Sen");
+	}
+	
+	private void Update () 
+    {
+		float distance = Vector2.Distance(transform.position, sen.transform.position);
+		if (distance < activationRange && Input.GetKeyDown(KeyCode.E))
+		{
+			IncreaseMaxHealth();
+		}		
+	}
+
+	private void IncreaseMaxHealth()
+	{
+		sen.GetComponent<PlayerHealth>().IncreaseMaxHealth(amountToIncreaseBy);
+		Destroy(gameObject);
+	}
+}
