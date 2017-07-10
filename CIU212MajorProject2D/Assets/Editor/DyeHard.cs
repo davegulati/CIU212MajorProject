0435cc5c3dee4754089bed71afb5fecf51d09ef4
Dave Gulati <1006106@student.sae.edu.au>
2017-07-10 10:10:39 +1000

    See commit message number 4.21 in dev log doc on google drive for more info.

--- CIU212MajorProject2D/Assets/Editor/DyeHard.cs
+++ CIU212MajorProject2D/Assets/Editor/DyeHard.cs
@@ -5,6 +5,7 @@ public class DyeHard : EditorWindow {
 
     public float timeScale = 1.0f;
 	public float healthToGive = 0;
+    public Object bud;
 
     [MenuItem("Window/Dye Hard Settings")]
     public static void ShowWindow()
@@ -16,33 +17,58 @@ public class DyeHard : EditorWindow {
     {
         GUILayout.Label("Fields marked with * are not yet functional.", EditorStyles.boldLabel);
         GUILayout.Label("");
+
 		GUILayout.Label("General Settings", EditorStyles.boldLabel);
         GUILayout.Label("Set timescale:", EditorStyles.label);
-		timeScale = EditorGUILayout.Slider(timeScale, 0.001f, 1.0f);
-		if (GUILayout.Button("Update Game Settings"))
-		{
-            Time.timeScale = timeScale;
-		}
-
+        GUILayout.BeginHorizontal();
+			timeScale = EditorGUILayout.Slider(timeScale, 0.001f, 1.0f);
+			if (GUILayout.Button("Set"))
+			{
+	            Time.timeScale = timeScale;
+			}
+        GUILayout.EndHorizontal();
 		GUILayout.Label("");
 
 		GUILayout.Label("Sen's Settings", EditorStyles.boldLabel);
-        healthToGive = EditorGUILayout.FloatField("Set Sen's health to:", healthToGive);
-
-        if (GUILayout.Button("Update Sen's Settings"))
-        {
-            GameObject.Find("Sen").GetComponent<PlayerHealth>().PlayerSetHealth(healthToGive);
-        }
-
+        GUILayout.BeginHorizontal();
+        healthToGive = EditorGUILayout.FloatField("Set Sen's current health to:", healthToGive);
+	        if (GUILayout.Button("Set"))
+	        {
+	            GameObject.Find("Sen").GetComponent<PlayerHealth>().PlayerSetHealth(healthToGive);
+	        }
+        GUILayout.EndHorizontal();
 		GUILayout.Label("");
 
 		GUILayout.Label("Bud's Settings", EditorStyles.boldLabel);
-		if (GUILayout.Button("Kill Bud"))
+        GUILayout.BeginHorizontal();
+	        GUILayout.Label("Select Bud prefab:", EditorStyles.label);
+	        bud = EditorGUILayout.ObjectField(bud, typeof(Object), true);
+		if (GUILayout.Button("Spawn Bud"))
 		{
-            GameObject bud = GameObject.Find("Bud");
-            Destroy(bud);
+			if (bud != null)
+			{
+				Instantiate(bud);
+			}
 		}
-
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+	        if (GUILayout.Button("Kill Bud (Main)"))
+			{
+				GameObject bud = GameObject.Find("Bud");
+	            if (bud != null)
+	            {
+	                Destroy(bud);
+	            }
+			}
+	        if (GUILayout.Button("Kill Bud (Clones)"))
+			{
+				bud = GameObject.Find("Bud(Clone)");
+	            if (bud != null)
+	            {
+	                Destroy(bud);
+	            }
+			}
+        GUILayout.EndHorizontal();
 		GUILayout.Label("");
 
 		GUILayout.Label("***Enemy Settings***", EditorStyles.boldLabel);
