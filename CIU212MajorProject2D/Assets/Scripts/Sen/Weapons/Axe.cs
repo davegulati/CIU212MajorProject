ef71ff67daf7504eae997617005a2fe920c61586
Dave Gulati <1006106@student.sae.edu.au>
2017-08-05 15:41:10 +1000

    Commit message number 4.61 in dev log.

---
+++ CIU212MajorProject2D/Assets/Scripts/Sen/Weapons/Axe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour {
+
+    // Damage Amounts
+    [HideInInspector]
+    public int damageAmount_GroundEnemy = 40;
+
+	void Update () 
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			//Attack
+		}		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "GroundEnemy")
+        {
+			if (collision.gameObject.GetComponent<GroundEnemyHealth>() != null)
+			{
+				collision.gameObject.GetComponent<GroundEnemyHealth>().DamageEnemy(damageAmount_GroundEnemy);
+			}
+        }
+    }
+}
