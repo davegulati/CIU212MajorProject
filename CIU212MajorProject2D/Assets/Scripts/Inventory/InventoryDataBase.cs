14f7834c602776d39b11002a76620074c40c8780
Hassain Hussein <1005114@student.sae.edu.au>
2017-08-08 20:45:06 +1000

    See commit message 4.8

---
+++ CIU212MajorProject2D/Assets/Scripts/Inventory/InventoryDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDataBase : MonoBehaviour
+{
+
+    public List<ITEM> items = new List<ITEM>();
+}
+
+[System.Serializable]
+
+public class ITEM
+{
+    public int ID;
+    public string name;
+    public Sprite image;
+}
---
+++ CIU212MajorProject2D/Assets/Scripts/Inventory/ItemSelf.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSelf : MonoBehaviour {
+
+    public int ID;
+    public int count;
+    public float health;
+}
