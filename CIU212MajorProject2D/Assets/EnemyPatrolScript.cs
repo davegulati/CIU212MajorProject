ebe13b90590da31fbc63cf1042b5620fbfb143c4
David Nikolovski <1005259@student.sae.edu.au>
2017-08-13 19:47:56 +1000

    reffer to commit message 4.92

---
+++ CIU212MajorProject2D/Assets/EnemyPatrolScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolScript : MonoBehaviour {
+
+	public Transform[] patrolpoints;
+	int currentPoint;
+	public float speed= 0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Patrol");	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	IEnumerable Patrol ()
+	{
+		while (true) {
+
+			if (transform.position.x == patrolpoints [currentPoint].position.x) 
+			
+			{
+				currentPoint++;
+			}		
+
+			if (currentPoint >= patrolpoints.Length)
+			{
+				currentPoint=0;
+			}
+
+			transform.position=Vector2.MoveTowards(transform.position,new Vector2(patrolpoints[currentPoint].position.x,transform.position.y),speed);
+
+
+
+			yield return null;
+
+
+					}
+
+
+	}
+
+}
