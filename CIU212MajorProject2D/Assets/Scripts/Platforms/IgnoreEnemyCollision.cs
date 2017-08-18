a5c894f5bfc2b8d3148d2215f522c0a76e3c1eb2
Dave Gulati <1006106@student.sae.edu.au>
2017-08-18 15:58:46 +1000

    Commit message number 5.03 in dev log.

---
+++ CIU212MajorProject2D/Assets/Scripts/Platforms/IgnoreEnemyCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreEnemyCollision : MonoBehaviour {
+
+    private GameObject[] groundEnemies;
+
+    private void Awake()
+    {
+        groundEnemies = GameObject.FindGameObjectsWithTag("GroundEnemy");
+        foreach (GameObject groundEnemy in groundEnemies)
+        {
+            Physics2D.IgnoreCollision(groundEnemy.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+        }
+    }
+}
