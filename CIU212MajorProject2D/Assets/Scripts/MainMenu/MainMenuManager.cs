b2c3282c5d350a3341db9a599461dc83ad1d0017
Dave Gulati <1006106@student.sae.edu.au>
2017-08-23 18:24:35 +1000

    Remade main menu.

---
+++ CIU212MajorProject2D/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public void Button_Play_Clicked ()
+    {
+        SceneManager.LoadScene("SafeZoneGreybox");
+    }
+
+    public void Button_Options_Clicked ()
+    {
+        
+    }
+
+    public void Button_Quit_Clicked ()
+    {
+        Application.Quit();
+    }
+}
