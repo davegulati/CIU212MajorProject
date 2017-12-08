636474d89714181b9aa87f4d7b13411ce4a0abec
Dave Gulati <1006106@student.sae.edu.au>
2017-12-08 16:37:30 +1100

    Commit message number  6.45 in dev log.

---
+++ CIU212MajorProject2D/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class CutsceneManager : MonoBehaviour 
+{
+    public MovieTexture cutscene;
+    private AudioSource cutsceneAudio;
+    public string sceneToLoad;
+
+    private void Start()
+    {
+        gameObject.GetComponent<RawImage>().texture = cutscene as MovieTexture;
+        cutsceneAudio = gameObject.GetComponent<AudioSource>();
+        cutsceneAudio.clip = cutscene.audioClip;
+        cutscene.Play();
+        cutsceneAudio.Play();
+    }
+
+    private void Update()
+    {
+        if (!cutscene.isPlaying)
+        {
+            CutsceneFinished();
+        }
+
+        if (Input.anyKeyDown)
+        {
+            Skip();
+        }
+    }
+
+    private void CutsceneFinished()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    private void Skip()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
