584addb8efdd938c3096096cc041ebee2157791b
Qxintero <1005829@student.sae.edu.au>
2017-12-08 17:51:33 +1100

    Commit message number 6.49

---
+++ CIU212MajorProject2D/Assets/Scripts/LoadLevel/LoadShadowCutscene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadShadowCutscene : MonoBehaviour
+{
+	private GameObject sen;
+	public GameObject ui;
+	private float activationRange = 3.5f;
+
+    private GameManager gameManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sen = GameObject.Find("Sen");
+//		ui = GameObject.Find("LoadJungle");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float distance = Vector2.Distance(transform.position, sen.transform.position);
+		if (distance < activationRange)
+		{
+			ui.gameObject.SetActive(true);
+		}
+
+		if (distance > activationRange)
+		{
+			ui.gameObject.SetActive(false);
+		}
+
+		if (distance < activationRange && Input.GetKeyDown(KeyCode.W))
+		{
+			gameManager.LoadScene("CutsceneSenVsShadowSen");
+		}
+	}
+}
