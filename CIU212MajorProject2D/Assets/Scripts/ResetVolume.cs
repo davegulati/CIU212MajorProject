be99328680e228217d9e19c30f1918c74de22506
Dave Gulati <1006106@student.sae.edu.au>
2017-12-11 17:12:51 +1100

    Commit message number 6.65 in dev log.

---
+++ CIU212MajorProject2D/Assets/Scripts/ResetVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetVolume : MonoBehaviour {
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 respawnPoint = GameObject.Find("RespawnPoint").transform.position;
+            collision.gameObject.transform.position = respawnPoint;
+        }
+    }
+}
